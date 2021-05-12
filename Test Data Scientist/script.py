830916f6b66ec8318623435b0920bee47a2d2627
Andrea Agostini <agostini95@gmail.com>
2021-05-12 11:49:40 +0200

    data

---
+++ Test Data Scientist/script.py
@@ -0,0 +1,19 @@
+#%%
+import pandas as pd
+import numpy as np
+
+#data loading
+df = pd.read_csv("data.csv")
+df.head
+df.shape
+df.columns
+df.info()
+df.describe()
+
+
+#pre processing
+
+
+
+
+# %%
